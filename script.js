735ffd14bbf3634d0edebff5b43fffff774c54b8
Kenny Morales <kennymorales580@gmail.com>
2023-07-06 05:46:15 -0500

    finished what I wanted to make

--- script.js
+++ script.js
@@ -2,62 +2,50 @@ let gameStorage = ["", "", "", "", "", "", "", "", ""];
 
 
 function userInput(cardNumber) {
-    if (gameStorage.includes("X", cardNumber - 1) == true)
+    console.log(cardNumber)
+    console.log(cardNumber-1)
+    if ((gameStorage[cardNumber-1] == "X") || (gameStorage[cardNumber-1] == "O"))
         console.log("error");
     else {
         switch (cardNumber) {
             case 1:
                 gameStorage[0] = "X";
-                gameMaster();
-                console.log(gameStorage);
                 break;
             case 2:
                 gameStorage[1] = "X";
-                gameMaster();
-                console.log(gameStorage);
                 break;
             case 3:
                 gameStorage[2] = "X";
-                gameMaster();
-                console.log(gameStorage);
                 break;
             case 4:
                 gameStorage[3] = "X";
-                gameMaster();
-                console.log(gameStorage);
                 break;
             case 5:
                 gameStorage[4] = "X";
-                gameMaster();
-                console.log(gameStorage);
                 break;
             case 6:
                 gameStorage[5] = "X";
-                gameMaster();
-                console.log(gameStorage);
                 break;
             case 7:
                 gameStorage[6] = "X";
-                gameMaster();
-                console.log(gameStorage);
                 break;
             case 8:
                 gameStorage[7] = "X";
-                gameMaster();
-                console.log(gameStorage);
                 break;
             case 9:
                 gameStorage[8] = "X";
-                gameMaster();
-                console.log(gameStorage);
                 break;
         }
+        gameMaster();
+        console.log(gameStorage);
+        updateDisplayTurns(cardNumber, "X");
     }
 }
 function computerInput() {
     for (k = 0; k < gameStorage.length; k++) {
         if (gameStorage[k] == "") {
             gameStorage[k] = "O";
+            updateDisplayTurns(k + 1, "O");
             break;
         }
     }
@@ -74,91 +62,111 @@ function gameMaster() {
 
 function checkForWinner() {
     if ((gameStorage[0] == "X") && (gameStorage[1] == "X") && (gameStorage[2] == "X")) {
-        console.log("Player won");
-        reset();
+        updateDisplayWinner("You Won!");
+        autoReset();
 
     }
     else if ((gameStorage[0] == "O") && (gameStorage[1] == "O") && (gameStorage[2] == "O")) {
-        console.log("Computer won");
-        reset();
+        updateDisplayWinner("You Lost To A Bot...");
+        autoReset();
         gameStorage = ["", "", "", "", "", "", "", "", ""];
     }
 
     else if ((gameStorage[3] == "X") && (gameStorage[4] == "X") && (gameStorage[5] == "X")) {
-        console.log("Player won");
-        reset();
+        updateDisplayWinner("You Won!");
+        autoReset();
     }
     else if ((gameStorage[3] == "O") && (gameStorage[4] == "O") && (gameStorage[5] == "O")) {
-        console.log("Computer won");
-        reset();
+        updateDisplayWinner("You Lost To A Bot...");
+        autoReset();
         gameStorage = ["", "", "", "", "", "", "", "", ""];
     }
 
     else if ((gameStorage[6] == "X") && (gameStorage[7] == "X") && (gameStorage[8] == "X")) {
-        console.log("Player won");
-        reset();
+        updateDisplayWinner("You Won!");
+        autoReset();
     }
     else if ((gameStorage[6] == "O") && (gameStorage[7] == "O") && (gameStorage[8] == "O")) {
-        console.log("Computer won");
-        reset();
+        updateDisplayWinner("You Lost To A Bot...");
+        autoReset();
         gameStorage = ["", "", "", "", "", "", "", "", ""];
     }
 
     else if ((gameStorage[0] == "X") && (gameStorage[3] == "X") && (gameStorage[6] == "X")) {
-        console.log("Player won");
-        reset();
+        updateDisplayWinner("You Won!");
+        autoReset();
     }
     else if ((gameStorage[0] == "O") && (gameStorage[3] == "O") && (gameStorage[6] == "O")) {
-        console.log("Computer won");
-        reset();
+        updateDisplayWinner("You Lost To A Bot...");
+        autoReset();
         gameStorage = ["", "", "", "", "", "", "", "", ""];
     }
 
     else if ((gameStorage[1] == "X") && (gameStorage[4] == "X") && (gameStorage[7] == "X")) {
-        console.log("Player won");
-        reset();
+        updateDisplayWinner("You Won!");
+        autoReset();
     }
     else if ((gameStorage[1] == "O") && (gameStorage[4] == "O") && (gameStorage[7] == "O")) {
-        console.log("Computer won");
-        reset();
+        updateDisplayWinner("You Lost To A Bot...");
+        autoReset();
         gameStorage = ["", "", "", "", "", "", "", "", ""];
     }
 
     else if ((gameStorage[2] == "X") && (gameStorage[5] == "X") && (gameStorage[8] == "X")) {
-        console.log("Player won");
-        reset();
+        updateDisplayWinner("You Won!");
+        autoReset();
     }
     else if ((gameStorage[2] == "O") && (gameStorage[5] == "O") && (gameStorage[8] == "O")) {
-        console.log("Computer won");
-        reset();
+        updateDisplayWinner("You Lost To A Bot...");
+        autoReset();
         gameStorage = ["", "", "", "", "", "", "", "", ""];
     }
 
     else if ((gameStorage[0] == "X") && (gameStorage[4] == "X") && (gameStorage[8] == "X")) {
-        console.log("Player won");
-        reset();
+        updateDisplayWinner("You Won!");
+        autoReset();
     }
     else if ((gameStorage[0] == "O") && (gameStorage[4] == "O") && (gameStorage[8] == "O")) {
-        console.log("Computer won");
-        reset();
+        updateDisplayWinner("You Lost To A Bot...");
+        autoReset();
         gameStorage = ["", "", "", "", "", "", "", "", ""];
     }
 
     else if ((gameStorage[2] == "X") && (gameStorage[4] == "X") && (gameStorage[6] == "X")) {
-        console.log("Player won");
-        reset();
+        updateDisplayWinner("You Won!");
+        autoReset();
     }
     else if ((gameStorage[2] == "O") && (gameStorage[4] == "O") && (gameStorage[6] == "O")) {
-        console.log("Computer won");
-        reset();
+        updateDisplayWinner("You Lost To A Bot...");
+        autoReset();
         gameStorage = ["", "", "", "", "", "", "", "", ""];
     }
 
     else {
-        console.log("game isn't done");
+        updateDisplayWinner("TicTacToe");
     }
 }
 
-function reset() {
+function autoReset() {
     gameStorage = ["rip"];
+}
+
+function forceReset() {
+    gameStorage = ["", "", "", "", "", "", "", "", ""];
+    for (k = 1; k < gameStorage.length + 1; k++) {
+        card = document.getElementById(k);
+        card.classList.add('playerTranslation');
+        card.textContent = '';
+    }
+    updateDisplayWinner("TicTacToe")
+    
+}
+
+function updateDisplayTurns(cardNumber, player) {
+    card = document.getElementById(cardNumber);
+    card.textContent = player;
+}
+function updateDisplayWinner(winner) {
+    card = document.getElementById("winner");
+    card.textContent = winner;
 }
