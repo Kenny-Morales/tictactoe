b607371a8e1c9e986b1eee7158a6eaa970d4e1b7
Kenny Morales <kennymorales580@gmail.com>
2023-07-06 03:08:33 -0500

    added computer input based on open slot

--- script.js
+++ script.js
@@ -1,46 +1,60 @@
 let gameStorage = ["", "", "", "", "", "", "", "", ""];
 
-function userInput(cardNumber, turn) {
+function userInput(cardNumber) {
     switch (cardNumber) {
         case 1:
-            gameStorage[0] = 1;
+            gameStorage[0] = "X";
+            computerInput();
             console.log(gameStorage);
             break;
         case 2:
-            gameStorage[1] = 2;
+            gameStorage[1] = "X";
+            computerInput();
             console.log(gameStorage);
             break;
         case 3:
-            gameStorage[2] = 3;
+            gameStorage[2] = "X";
+            computerInput();
             console.log(gameStorage);
             break;
         case 4:
-            gameStorage[3] = 4;
+            gameStorage[3] = "X";
+            computerInput();
             console.log(gameStorage);
             break;
         case 5:
-            gameStorage[4] = 5;
+            gameStorage[4] = "X";
+            computerInput();
             console.log(gameStorage);
             break;
         case 6:
-            gameStorage[5] = 6;
+            gameStorage[5] = "X";
+            computerInput();
             console.log(gameStorage);
             break;
         case 7:
-            gameStorage[6] = 7;
+            gameStorage[6] = "X";
+            computerInput();
             console.log(gameStorage);
             break;
         case 8:
-            gameStorage[7] = 8;
+            gameStorage[7] = "X";
+            computerInput();
             console.log(gameStorage);
             break;
         case 9:
-            gameStorage[8] = 9;
+            gameStorage[8] = "X";
+            computerInput();
             console.log(gameStorage);
             break;
     }
 }
 
 function computerInput() {
-
+    for (k = 0; k < gameStorage.length; k++) { 
+        if (gameStorage[k] == "") {
+            gameStorage[k] = "O";
+            break;
+        }
+    }
 }
