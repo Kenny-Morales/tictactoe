ae62d612f31a78fd6aabd6d5262fe460b222d493
Kenny Morales <kennymorales580@gmail.com>
2023-07-06 02:18:42 -0500

    made each thing push to the array like a g

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 let gameStorage = ["", "", "", "", "", "", "", "", ""];
 
 function userInput(cardNumber) {
-    console.log(cardNumber + " was clicked")
+    switch (cardNumber) {
+        case 1:
+            gameStorage[0] = 1;
+            console.log(gameStorage);
+            break;
+        case 2:
+            gameStorage[1] = 2;
+            console.log(gameStorage);
+            break;
+        case 3:
+            gameStorage[2] = 3;
+            console.log(gameStorage);
+            break;
+        case 4:
+            gameStorage[3] = 4;
+            console.log(gameStorage);
+            break;
+        case 5:
+            gameStorage[4] = 5;
+            console.log(gameStorage);
+            break;
+        case 6:
+            gameStorage[5] = 6;
+            console.log(gameStorage);
+            break;
+        case 7:
+            gameStorage[6] = 7;
+            console.log(gameStorage);
+            break;
+        case 8:
+            gameStorage[7] = 8;
+            console.log(gameStorage);
+            break;
+        case 9:
+            gameStorage[8] = 9;
+            console.log(gameStorage);
+            break;
+    }
 }
 
 function playerTurn() {
