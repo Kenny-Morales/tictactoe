1580c655a5a2a619684dbda54ba5aa900cdbc72d
Kenny Morales <kennymorales580@gmail.com>
2023-07-06 04:02:30 -0500

    added game winner logic after 100 years

--- script.js
+++ script.js
@@ -1,75 +1,139 @@
 let gameStorage = ["", "", "", "", "", "", "", "", ""];
 
-function userInput(cardNumber) {
-    switch (cardNumber) {
-        case 1:
-            gameStorage[0] = "X";
-            computerInput();
-            winner();
-            console.log(gameStorage);
-            break;
-        case 2:
-            gameStorage[1] = "X";
-            computerInput();
-            winner();
-            console.log(gameStorage);
-            break;
-        case 3:
-            gameStorage[2] = "X";
-            computerInput();
-            winner();
-            console.log(gameStorage);
-            break;
-        case 4:
-            gameStorage[3] = "X";
-            computerInput();
-            winner();
-            console.log(gameStorage);
-            break;
-        case 5:
-            gameStorage[4] = "X";
-            computerInput();
-            winner();
-            console.log(gameStorage);
-            break;
-        case 6:
-            gameStorage[5] = "X";
-            computerInput();
-            winner();
-            console.log(gameStorage);
-            break;
-        case 7:
-            gameStorage[6] = "X";
-            computerInput();
-            winner();
-            console.log(gameStorage);
-            break;
-        case 8:
-            gameStorage[7] = "X";
-            computerInput();
-            winner();
-            console.log(gameStorage);
-            break;
-        case 9:
-            gameStorage[8] = "X";
-            computerInput();
-            winner();
-            console.log(gameStorage);
-            break;
+function userInput(cardNumber, done) {
+    if (done == "game is done")
+        reset();
+    else {
+        switch (cardNumber) {
+            case 1:
+                gameStorage[0] = "X";
+                checkForWinner();
+                computerInput();
+                console.log(gameStorage);
+                break;
+            case 2:
+                gameStorage[1] = "X";
+                checkForWinner();
+                computerInput();
+                console.log(gameStorage);
+                break;
+            case 3:
+                gameStorage[2] = "X";
+                checkForWinner();
+                computerInput();
+                console.log(gameStorage);
+                break;
+            case 4:
+                gameStorage[3] = "X";
+                checkForWinner();
+                computerInput();
+                console.log(gameStorage);
+                break;
+            case 5:
+                gameStorage[4] = "X";
+                checkForWinner();
+                computerInput();
+                console.log(gameStorage);
+                break;
+            case 6:
+                gameStorage[5] = "X";
+                checkForWinner();
+                computerInput();
+                console.log(gameStorage);
+                break;
+            case 7:
+                gameStorage[6] = "X";
+                checkForWinner();
+                computerInput();
+                console.log(gameStorage);
+                break;
+            case 8:
+                gameStorage[7] = "X";
+                checkForWinner();
+                computerInput();
+                console.log(gameStorage);
+                break;
+            case 9:
+                gameStorage[8] = "X";
+                checkForWinner();
+                computerInput();
+                console.log(gameStorage);
+                break;
+        }
     }
+    
 }
 
 function computerInput() {
     for (k = 0; k < gameStorage.length; k++) { 
         if (gameStorage[k] == "") {
             gameStorage[k] = "O";
+            checkForWinner();
             break;
         }
     }
 }
 
-function winner() {
-    if ((gameStorage[0] && gameStorage[3] && gameStorage[6]) == "X") {
+function checkForWinner() {
+    if ((gameStorage[0] == "X") && (gameStorage[1] == "X") && (gameStorage[2] == "X")) {
         console.log("Player won");
     }
+    else if((gameStorage[0] == "O") && (gameStorage[1] == "O") && (gameStorage[2] == "O")) {
+        console.log("Computer won");
+    }
+        
+    else if ((gameStorage[3] == "X") && (gameStorage[4] == "X") && (gameStorage[5] == "X")) {
+        console.log("Player won");
+    }
+    else if((gameStorage[3] == "O") && (gameStorage[4] == "O") && (gameStorage[5] == "O")) {
+        console.log("Computer won");
+    }
+
+    else if ((gameStorage[6] == "X") && (gameStorage[7] == "X") && (gameStorage[8] == "X")) {
+        console.log("Player won");
+    }
+    else if((gameStorage[6] == "O") && (gameStorage[7] == "O") && (gameStorage[8] == "O")) {
+        console.log("Computer won");
+    }
+    
+    else if ((gameStorage[0] == "X") && (gameStorage[3] == "X") && (gameStorage[6] == "X")) {
+        console.log("Player won");
+    }
+    else if((gameStorage[0] == "O") && (gameStorage[3] == "O") && (gameStorage[6] == "O")) {
+        console.log("Computer won");
+    }
+
+    else if ((gameStorage[1] == "X") && (gameStorage[4] == "X") && (gameStorage[7] == "X")) {
+        console.log("Player won");
+    }
+    else if((gameStorage[1] == "O") && (gameStorage[4] == "O") && (gameStorage[7] == "O")) {
+        console.log("Computer won");
+    }
+
+    else if ((gameStorage[2] == "X") && (gameStorage[5] == "X") && (gameStorage[8] == "X")) {
+        console.log("Player won");
+    }
+    else if((gameStorage[2] == "O") && (gameStorage[5] == "O") && (gameStorage[8] == "O")) {
+        console.log("Computer won");
+    }
+    
+    else if ((gameStorage[0] == "X") && (gameStorage[4] == "X") && (gameStorage[8] == "X")) {
+        console.log("Player won");
+    }
+    else if((gameStorage[0] == "O") && (gameStorage[4] == "O") && (gameStorage[8] == "O")) {
+        console.log("Computer won");
+    }
+        
+    else if ((gameStorage[2] == "X") && (gameStorage[4] == "X") && (gameStorage[6] == "X")) {
+        console.log("Player won");
+    }
+    else if((gameStorage[2] == "O") && (gameStorage[4] == "O") && (gameStorage[6] == "O")) {
+        console.log("Computer won");
+    }
+   
+    else {
+        console.log("game isn't done");
+    }
+    
+
 }
