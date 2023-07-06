c79b5bf1d6104a9ab42c2d6e98484cdbe4c28305
Kenny Morales <kennymorales580@gmail.com>
2023-07-06 03:15:25 -0500

    added winning logic

--- script.js
+++ script.js
@@ -5,46 +5,55 @@ function userInput(cardNumber) {
         case 1:
             gameStorage[0] = "X";
             computerInput();
+            winner();
             console.log(gameStorage);
             break;
         case 2:
             gameStorage[1] = "X";
             computerInput();
+            winner();
             console.log(gameStorage);
             break;
         case 3:
             gameStorage[2] = "X";
             computerInput();
+            winner();
             console.log(gameStorage);
             break;
         case 4:
             gameStorage[3] = "X";
             computerInput();
+            winner();
             console.log(gameStorage);
             break;
         case 5:
             gameStorage[4] = "X";
             computerInput();
+            winner();
             console.log(gameStorage);
             break;
         case 6:
             gameStorage[5] = "X";
             computerInput();
+            winner();
             console.log(gameStorage);
             break;
         case 7:
             gameStorage[6] = "X";
             computerInput();
+            winner();
             console.log(gameStorage);
             break;
         case 8:
             gameStorage[7] = "X";
             computerInput();
+            winner();
             console.log(gameStorage);
             break;
         case 9:
             gameStorage[8] = "X";
             computerInput();
+            winner();
             console.log(gameStorage);
             break;
     }
@@ -56,5 +65,11 @@ function computerInput() {
             gameStorage[k] = "O";
             break;
         }
+    }
+}
+
+function winner() {
+    if ((gameStorage[0] && gameStorage[3] && gameStorage[6]) == "X") {
+        console.log("Player won");
     }
 }
